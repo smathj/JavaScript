0850c72fffa36e1312bea5ba0b34931527462377
NaTaeKoon <smathj@naver.com>
2021-09-23 23:15:41 +0900

    JavaScript - 실행 컨텍스트 Study

---
+++ 실행컨텍스트/스코프체인.js
@@ -0,0 +1,17 @@
+var a = 1;
+var outer = function() {
+    var inner = function() {
+        console.log(a);
+        var a = 3;
+    };
+    inner();
+    console.log(a);
+};
+
+outer();
+console.log(a);
+
+
+// var a
+// var outer = function outer() {...}
+// var inner = function inner() { var a }
---
+++ 실행컨텍스트/호이스팅2.js
@@ -0,0 +1,9 @@
+function a() {
+    var x = 1;
+    console.log(x);     // 1
+    var x;              
+    console.log(x);     // 1
+    var x =2;           
+    console.log(x);     // 2
+}
+a(1)
